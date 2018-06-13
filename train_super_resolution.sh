eacd042ab4acee71c0c29be47e71ec570d970d1f
Aneesh Karve <aneesh.karve@gmail.com>
2018-06-13 16:03:13 -0700

    add requirements.txt

--- train_super_resolution.sh
+++ train_super_resolution.sh
@@ -2,15 +2,15 @@
 # USAGE: bash train_super_resolution.sh N_EPOCHS
 # Entrypoint for Paperspace Job
 
+# requirements
+pip install -r super_resolution/requirements.txt
 # read/write Quilt packages from/to persistent storage
 export QUILT_PRIMARY_PACKAGE_DIR='/storage/quilt_modules'
-
 cd super_resolution
-
+# default to 10 epochs (prefer ~500)
 N_EPOCHS=$1
-# Default to 10
 echo "Training for ${N_EPOCHS:=10} epochs\n"
-
+# train
 python main.py \
 	--upscale_factor 3 \
 	--batchSize 4 \
