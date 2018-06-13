19ad92f44f7416257070c8ec02cb3f6003879baf
Aneesh Karve <aneesh.karve@gmail.com>
2018-06-13 15:40:36 -0700

    parameterize n_epochs

--- train_super_resolution.sh
+++ train_super_resolution.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+# USAGE: bash train_super_resolution.sh N_EPOCHS
 # Entrypoint for Paperspace Job
 
 # read/write Quilt packages from/to persistent storage
@@ -6,10 +7,13 @@ export QUILT_PRIMARY_PACKAGE_DIR='/storage/quilt_modules'
 
 cd super_resolution
 
+# Default to 10 epochs
+$1 := 10
+
 python main.py \
 	--upscale_factor 3 \
 	--batchSize 4 \
 	--testBatchSize 100 \
-	--nEpochs 10 \
+	--nEpochs $1 \
 	--lr 0.001 \
 	--cuda
