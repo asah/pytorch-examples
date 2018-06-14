e9cdd50fc62d993c778d3989712abd3e14aa66fe
Aneesh Karve <aneesh.karve@gmail.com>
2018-06-13 18:44:02 -0700

    add inference script

---
+++ infer_super_resolution.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# USAGE: bash infer_super_resolution.sh EPOCH TRAIN_IMAGE_ID
+# Entrypoint for Paperspace Job
+
+# read/write Quilt packages from/to persistent storage
+export QUILT_PRIMARY_PACKAGE_DIR='/storage/quilt_packages'
+# working directory
+cd super_resolution
+# requirements
+pip install -r requirements.txt
+
+quilt export akarve/BSDS300/images/test/n$2 .
+
+python super_resolve.py \
+	--cuda
+	--input_image mesmall.jpg \
+	--model /storage/models/super_resolution/model_epoch_$1.pth \
+	--output_filename /artifacts/super-$2.png
